6cfdc8f022657fc64c16ed33543794a5904f32b6
Tushar Dontulwar <tushdontulwar@gmail.com>
2025-01-10 00:25:28 +0530

    Edited Island and nav

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -98,9 +98,9 @@ const Island = ({isRotating, setIsRotating, setCurrentStage, ...props})=> {
       const clientX = e.touches ? e.touches[0].clientX : e.clientX;
       const delta = (clientX - lastX.current) / viewport.width;
   
-      islandRef.current.rotation.y += delta * 0.01 * Math.PI;
+      islandRef.current.rotation.y += delta * 0.005 * Math.PI;
       lastX.current = clientX;
-      rotationSpeed.current = delta * 0.01 * Math.PI;
+      rotationSpeed.current = delta * 0.005 * Math.PI;
     }
   }
 
